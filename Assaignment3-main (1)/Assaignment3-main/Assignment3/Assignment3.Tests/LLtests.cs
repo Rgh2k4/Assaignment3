08218067aa7e40ad37e381e0f9f4fdaee973e938
Rgh2k4 <146884367+Rgh2k4@users.noreply.github.com>
2024-04-04 23:48:18 -0600

    Update LLtests.cs

--- Assaignment3-main (1)/Assaignment3-main/Assignment3/Assignment3.Tests/LLtests.cs
+++ Assaignment3-main (1)/Assaignment3-main/Assignment3/Assignment3.Tests/LLtests.cs
@@ -5,24 +5,30 @@ namespace LinkedListTests
     [TestFixture]
     public class Tests
     {
-        SinglyLinkedList users = new SinglyLinkedList();
+        public LinkedList TestList;
         [SetUp]
         public void Setup()
         {
+            TestList = new LinkedList();
+        }
 
+        [TearDown]
+        public void TearDown()
+        {
+            TestList.Clear();
         }
 
         [Test]
         public void IsEmpty_True_LinkedListIsEmpty()
         {
-            Assert.IsTrue(users.IsEmpty());
+            Assert.IsTrue(TestList.IsEmpty());
         }
 
         [Test]
         public void IsEmpty_False_LinkedListIsNotEmpty()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
-            Assert.IsFalse(users.IsEmpty());
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            Assert.IsFalse(TestList.IsEmpty());
         }
 
 
@@ -31,38 +37,38 @@ namespace LinkedListTests
         [Test]
         public void Add_GreaterThan0_NodeIsAdded()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
-            users.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
-            users.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
+            TestList.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
 
 
-            users.Add(new User(4, "Ronald McDonald", "burgers4life63@outlook.com", "mcdonalds999"), 2);
+            TestList.Add(new User(4, "Ronald McDonald", "burgers4life63@outlook.com", "mcdonalds999"), 2);
 
 
-            Assert.AreEqual("Ronald McDonald", users.GetValue(2).Name);
-            Assert.AreEqual(3, users.Count());
+            Assert.AreEqual("Ronald McDonald", TestList.GetValue(2).Name);
+            Assert.AreEqual(4, TestList.Count());
         }
 
         [Test]
         public void Add_LessThanCount_ExceptionThrown()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
-            users.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
-            users.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
+            TestList.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
 
 
-            Assert.Throws<IndexOutOfRangeException>(() => users.Add(new User(4, "Ronald McDonald", "burgers4life63@outlook.com", "mcdonalds999"), -2));
+            Assert.Throws<IndexOutOfRangeException>(() => TestList.Add(new User(4, "Ronald McDonald", "burgers4life63@outlook.com", "mcdonalds999"), -2));
         }
 
         [Test]
         public void Add_GreaterThanCount_ExceptionThrown()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
-            users.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
-            users.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
+            TestList.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
 
 
-            Assert.Throws<IndexOutOfRangeException>(() => users.Add(new User(4, "Ronald McDonald", "burgers4life63@outlook.com", "mcdonalds999"), 10));
+            Assert.Throws<IndexOutOfRangeException>(() => TestList.Add(new User(4, "Ronald McDonald", "burgers4life63@outlook.com", "mcdonalds999"), 10));
         }
 
 
@@ -71,36 +77,36 @@ namespace LinkedListTests
         [Test]
         public void Replace_GreaterThan0_NodeisReplaced()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
-            users.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
-            users.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
+            TestList.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
 
 
-            users.Replace(new User(4, "Ronald McDonald", "burgers4life63@outlook.com", "mcdonalds999"), 1);
+            TestList.Replace(new User(4, "Ronald McDonald", "burgers4life63@outlook.com", "mcdonalds999"), 1);
 
-            Assert.AreEqual("Ronald McDonald", users.GetValue(1).Name);
+            Assert.AreEqual("Ronald McDonald", TestList.GetValue(1).Name);
         }
 
         [Test]
         public void Replace_LessThan0_ExceptionThrown()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
-            users.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
-            users.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
+            TestList.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
 
 
-            Assert.Throws<IndexOutOfRangeException>(() => users.Replace(new User(4, "Ronald McDonald", "burgers4life63@outlook.com", "mcdonalds999"), -2));
+            Assert.Throws<IndexOutOfRangeException>(() => TestList.Replace(new User(4, "Ronald McDonald", "burgers4life63@outlook.com", "mcdonalds999"), -2));
         }
 
         [Test]
         public void Replace_GreaterThanCount_ExceptionThrown()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
-            users.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
-            users.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
+            TestList.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
 
 
-            Assert.Throws<IndexOutOfRangeException>(() => users.Replace(new User(4, "Ronald McDonald", "burgers4life63@outlook.com", "mcdonalds999"), 6));
+            Assert.Throws<IndexOutOfRangeException>(() => TestList.Replace(new User(4, "Ronald McDonald", "burgers4life63@outlook.com", "mcdonalds999"), 6));
         }
 
 
@@ -109,28 +115,28 @@ namespace LinkedListTests
         [Test]
         public void RemoveFirst_OnlyOneNode_HeadNotEmpty_NodeRemoved()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
 
-            users.RemoveFirst();
+            TestList.RemoveFirst();
 
-            Assert.IsTrue(users.IsEmpty());
+            Assert.IsTrue(TestList.IsEmpty());
         }
 
         [Test]
         public void RemoveFirst_MoreThanOneNode_HeadNotEmpty_NodeRemoved()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
-            users.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
 
-            users.RemoveFirst();
+            TestList.RemoveFirst();
 
-            Assert.AreEqual("Joe Schmoe", users.GetValue(1).Name);
+            Assert.AreEqual("Joe Schmoe", TestList.GetValue(0).Name);
         }
 
         [Test]
         public void RemoveFirst_HeadIsEmpty_ExceptionThrown()
         {
-            Assert.Throws<InvalidOperationException>(() => users.RemoveFirst());
+            Assert.Throws<InvalidOperationException>(() => TestList.RemoveFirst());
         }
 
 
@@ -139,27 +145,27 @@ namespace LinkedListTests
         [Test]
         public void RemoveLast_OnlyOneNode_HeadNotEmpty_NodeRemoved()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
 
-            users.RemoveLast();
+            TestList.RemoveLast();
 
-            Assert.IsTrue(users.IsEmpty());
+            Assert.IsTrue(TestList.IsEmpty());
         }
 
         [Test]
         public void RemoveLast_MoreThanOneNode_HeadNotEmpty_NodeRemoved()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
-            users.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
-            users.RemoveLast();
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
+            TestList.RemoveLast();
 
-            Assert.IsTrue(users.Head.Next.Next == null);
+            Assert.IsTrue(TestList.Head.Next == null);
         }
 
         [Test]
         public void RemoveLast_HeadIsEmpty_ExceptionThrown()
         {
-            Assert.Throws<InvalidOperationException>(() => users.RemoveLast());
+            Assert.Throws<InvalidOperationException>(() => TestList.RemoveLast());
         }
 
 
@@ -167,67 +173,67 @@ namespace LinkedListTests
         [Test]
         public void Remove_HeadNotEmpty_GreaterThan0_OnlyOneNode_NodeRemoved()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
 
-            users.Remove(1);
+            TestList.Remove(0);
 
-            Assert.IsTrue(users.IsEmpty());
+            Assert.IsTrue(TestList.IsEmpty());
         }
 
         [Test]
         public void Remove_HeadNotEmpty_LessThanCount_OnlyOneNode_NodeRemoved()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
 
-            Assert.Throws<IndexOutOfRangeException>(() => users.Remove(-1));
+            Assert.Throws<IndexOutOfRangeException>(() => TestList.Remove(-1));
         }
 
         [Test]
         public void Remove_HeadNotEmpty_GreaterThanCount_OnlyOneNode_NodeRemoved()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
 
-            Assert.Throws<IndexOutOfRangeException>(() => users.Remove(5));
+            Assert.Throws<IndexOutOfRangeException>(() => TestList.Remove(5));
         }
 
         [Test]
         public void Remove_HeadNotEmpty_GreaterThan0_MoreThanOneNode_NodeRemoved()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
-            users.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
-            users.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
-            users.Remove(2);
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
+            TestList.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
+            TestList.Remove(1);
 
-            Assert.AreEqual("Colonel Sanders", users.GetValue(2).Name);
-            Assert.AreEqual(3, users.Count());
+            Assert.AreEqual("Colonel Sanders", TestList.GetValue(1).Name);
+            Assert.AreEqual(2, TestList.Count());
         }
 
         [Test]
         public void Remove_HeadNotEmpty_LessThan0_MoreThanOneNode_NodeRemoved()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
-            users.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
-            users.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
-            users.Remove(2);
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
+            TestList.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
+            TestList.Remove(2);
 
-            Assert.Throws<IndexOutOfRangeException>(() => users.Remove(-1));
+            Assert.Throws<IndexOutOfRangeException>(() => TestList.Remove(-1));
         }
 
         [Test]
         public void Remove_HeadNotEmpty_GreaterThanCount_MoreThanOneNode_NodeRemoved()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
-            users.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
-            users.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
-            users.Remove(2);
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
+            TestList.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
+            TestList.Remove(2);
 
-            Assert.Throws<IndexOutOfRangeException>(() => users.Remove(5));
+            Assert.Throws<IndexOutOfRangeException>(() => TestList.Remove(5));
         }
 
         [Test]
         public void Remove_HeadIsEmpty_ExceptionThrown()
         {
-            Assert.Throws<IndexOutOfRangeException>(() => users.Remove(1));
+            Assert.Throws<IndexOutOfRangeException>(() => TestList.Remove(1));
         }
 
 
@@ -237,37 +243,37 @@ namespace LinkedListTests
         [Test]
         public void GetValue_HeadIsNotEmpty_GreaterThan0_ValueReturned()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
-            users.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
-            users.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
+            TestList.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
 
-            Assert.AreEqual("Joe Schmoe", users.GetValue(2).Name);
+            Assert.AreEqual("Joe Schmoe", TestList.GetValue(1).Name);
         }
 
         [Test]
         public void GetValue_HeadIsNotEmpty_LessThan0_ExceptionThrown()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
-            users.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
-            users.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
+            TestList.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
 
-            Assert.Throws<IndexOutOfRangeException>(() => users.GetValue(-1));
+            Assert.Throws<IndexOutOfRangeException>(() => TestList.GetValue(-1));
         }
 
         [Test]
         public void GetValue_HeadIsNotEmpty_GreaterThanCount_ExceptionThrown()
         {
-            users.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
-            users.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
-            users.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
+            TestList.AddLast(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            TestList.AddLast(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
+            TestList.AddLast(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
 
-            Assert.Throws<IndexOutOfRangeException>(() => users.GetValue(5));
+            Assert.Throws<IndexOutOfRangeException>(() => TestList.GetValue(5));
         }
 
         [Test]
         public void GetValue_HeadIsEmpty_ExceptionThrown()
         {
-            Assert.Throws<IndexOutOfRangeException>(() => users.GetValue(1));
+            Assert.Throws<IndexOutOfRangeException>(() => TestList.GetValue(1));
         }
 
     }
